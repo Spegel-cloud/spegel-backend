75c8510fad8dbb5e93f7774ce9402276561dbe5a
John-Erik Ågotnes <dagros@broadpark.no>
2025-08-11 18:20:42 +0200

    Force DATABASE_URL with SSL and clean server config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,11 @@
-// config/database.js
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      // Render Postgres connection (bruker én env: DATABASE_URL)
       connectionString: env('DATABASE_URL'),
-      ssl: {
-        // Render krever SSL; sertifikatet er ikke nødvendigvis “CA-signet”
-        rejectUnauthorized: false,
-      },
+      ssl: { rejectUnauthorized: false },
     },
-    pool: {
-      min: 0,
-      max: 10,
-    },
-    // Sett til true hvis du vil debugge SQL i loggene
+    pool: { min: 0, max: 10 },
     debug: false,
   },
 });
