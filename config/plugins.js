43aa711b211f37c7471dd8b6587713ecab122fb6
John-Erik Ågotnes <dagros@broadpark.no>
2025-08-12 23:04:11 +0200

    Add Cloudinary config with console log

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,22 @@
 // config/plugins.js
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        // valgfritt – gir orden i Cloudinary
-        folder: env('CLOUDINARY_UPLOAD_FOLDER', 'spegel'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
+module.exports = ({ env }) => {
+  console.log('[Cloudinary]', env('CLOUDINARY_NAME')); // Logger cloud_name ved oppstart
+
+  return {
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+          folder: env('CLOUDINARY_UPLOAD_FOLDER', 'spegel'), // valgfritt
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
       },
     },
-  },
-});
+  };
+};
